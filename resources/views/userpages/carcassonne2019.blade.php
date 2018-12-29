8365f34db5eeac3e19192e7e44f781d0df7e2412
Jan-Willem de Boer <janwillem@skrepr.com>
2018-12-29 17:45:02 +0100

    carcasonne pagina

--- resources/views/userpages/carcassonne2019.blade.php
+++ resources/views/userpages/carcassonne2019.blade.php
@@ -3,3 +3,35 @@
 @section('content')
     Carcassonne 2019
 @endsection
+
+@section('tekst')
+<div class="row">
+    <div class="col-sm text-center">
+        26 Maart 2019 <br>
+        Windesheim Flevoland <br>
+        van het Padje 19
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm text-center">
+        <img class="img-fluid" src="https://yoors-media-uploads-adsfairbv.netdna-ssl.com/5387/images/carcasonnespelbord.jpg">
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm text-center">
+        <h2>
+            <a href="/inschrijven">Inschrijven</a>
+        </h2>
+        <h2>
+            <a href="/toernooischema">
+                Toernooischema
+            </a>
+        </h2>
+        <h2>
+            <a href="/ranglijst">
+                Ranglijst
+            </a>
+        </h2>
+    </div>
+</div>
+@endsection
